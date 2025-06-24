76f693be31ac6b0834ef0c3e1cf271b63f0e73f9
Sowparna Shome <soupornoshome@gmail.com>
2025-06-25 00:50:49 +0530

    fix 1

--- app/db_conenct.py
+++ app/db_conenct.py
@@ -21,19 +21,19 @@ tmpPostgres = urlparse(DATABASE_URL)
 engine = create_async_engine(f"postgresql+asyncpg://{tmpPostgres.username}:{tmpPostgres.password}@{tmpPostgres.hostname}{tmpPostgres.path}?ssl=require", echo=True)
 # engine = create_async_engine(DATABASE_URL, future=True, echo=True)
 SessionLocal=sessionmaker(autocommit=False,autoflush=False,bind=engine,class_=AsyncSession)
-# Base=declarative_base()
+Base=declarative_base()
 async def get_db_session():
     async with SessionLocal() as session:
         yield session
 
-async def main():
-    gen = get_db_session()
-    try:
-        session = await gen.__anext__()
-        print("Got session:", session)
-        # You can now use the session
-    finally:
-        await gen.aclose()
+# async def main():
+#     gen = get_db_session()
+#     try:
+#         session = await gen.__anext__()
+#         print("Got session:", session)
+#         # You can now use the session
+#     finally:
+#         await gen.aclose()
 
-if __name__ == "__main__":
-    asyncio.run(main())
+# if __name__ == "__main__":
+#     asyncio.run(main())
